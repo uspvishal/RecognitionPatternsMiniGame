83f655f485de0726d9977210b5b24d34e29d6d8a
VLx <usp.vishal@gmail.com>
2026-06-15 18:23:11 +0530

    changes

---
+++ Assets/Scripts/MainLevelScript.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace USP.MiniGame.recognitionPatterns
+{
+    /// <summary>
+    /// Script by Vishal Lakhani
+    /// Email: usp.vishal@gmail.com
+    /// Description:
+    /// </summary>
+    public class MainLevelScript : MonoBehaviour
+    {
+        #region Variables
+
+
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+    }
+}
